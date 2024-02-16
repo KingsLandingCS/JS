04cbc1beb1626fee74f889cddc38f509654dc43c
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-02-16 05:43:20 +0500

    learning

---
+++ JS-promises/main.js
@@ -0,0 +1,33 @@
+const list = document.querySelector(".list");
+
+
+async function fetchListOfData() {
+
+    try {
+
+        const response = await fetch("https://dummyjson.com/quotes/random",
+            {
+                method: "GET"
+            })
+
+        const result = await response.json();
+
+        console.log(result);
+
+
+        if (result && result.products && result.products.length) renderProducts(result.products)
+
+    } catch (e) {
+
+        console.log(e);
+    }
+
+}
+
+function renderProducts(getProducts) {
+
+    list.innerHTML = getProducts.map((item) => `<p>${item.title}</p>`).join(" ");
+}  
+
+
+fetchListOfData();
