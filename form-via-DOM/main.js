8db7308a5353c3a2dccd95af75c971ec31c7718e
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-02-18 13:36:59 +0500

    learning

--- form-via-DOM/main.js
+++ form-via-DOM/main.js
@@ -1,6 +1,25 @@
+let form = document.createElement("form");
+form.setAttribute("id", "myForm");
+
+
+let input1 = document.createElement("input");
+input1.setAttribute("type", "text");
+input1.setAttribute("name", "firstname");
+input1.setAttribute("placeholder", "First Name");
+form.appendChild(input1);
+
+
+let input2 = document.createElement("input");
+input2.setAttribute("type", "text");
+input2.setAttribute("name", "lastName");
+input2.setAttribute("placeholder", "Last Name");
+form.appendChild(input2);
+
+let submitButton = document.createElement("input");
+submitButton.setAttribute("type", "submit");
+submitButton.setAttribute("value", "submit");
+form.appendChild(submitButton);
+
+
 const formContainer = document.querySelector(".form-container");
-
-
-
-formContainer.createElement("form");
-formContainer.createElement("input");
+formContainer.appendChild(form);
