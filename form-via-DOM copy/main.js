51e51b8a1e1eb1c40a19e5c1bf7da7f2d326465f
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-02-18 15:59:59 +0500

    learning

---
+++ form-via-DOM copy/main.js
@@ -0,0 +1,36 @@
+const form = document.createElement("form");
+form.setAttribute("id", "myForm");
+
+const label1 = document.createElement("label");
+label1.innerHTML = "First Name";
+label1.style.display = "block";
+form.appendChild(label1);
+
+const input1 = document.createElement("input");
+input1.setAttribute("type", "text");
+input1.setAttribute("name", "fname");
+input1.setAttribute("placeholder", "First Name");
+input1.style.marginBottom = "10px";
+form.appendChild(input1);
+
+const label2 = document.createElement("label");
+label2.innerHTML = "Last Name";
+label2.style.display = "block";
+form.appendChild(label2);
+
+const input2 = document.createElement("input");
+input2.setAttribute("type", "text");
+input2.setAttribute("name", "lname");
+input2.setAttribute("placeholder", "Last Name");
+input2.style.marginBottom = "10px";
+form.appendChild(input2);
+
+const submitButton = document.createElement("input");
+submitButton.setAttribute("type", "submit");
+submitButton.setAttribute("name", "submit");
+submitButton.style.marginTop = "10px";
+form.appendChild(submitButton);
+
+
+const formContainer = document.querySelector(".form-container");
+formContainer.appendChild(form);
