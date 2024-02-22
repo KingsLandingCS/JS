5d3690aa2630659bb30cb18b52b81e6ad3b7744a
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-02-22 07:34:44 +0500

    learning

---
+++ beginner-level-problems/main copy 12.js
@@ -0,0 +1,21 @@
+function isPrime(n) {
+
+   if (n < 2) {
+
+      return `${n} is not a prime number`;
+   }
+
+   for (i = 2; i < n; i++) {
+
+      if (n % i === 0) {
+
+         return `${n} is not a prime number`;
+      }
+
+   }
+   return `${n} is a prime a number`;
+}
+
+
+
+console.log(isPrime(9));
