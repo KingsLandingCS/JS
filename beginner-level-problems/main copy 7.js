5148ccd0a839d4423aaffe3f564959393ae8a7e6
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-02-22 05:42:53 +0500

    learning

---
+++ beginner-level-problems/main copy 7.js
@@ -0,0 +1,16 @@
+function checkEvenOddFn(num){
+
+    if(num % 2 === 0){
+
+        console.log("Even");
+    } 
+
+    else {
+
+        console.log("odd");
+    }
+
+    return num;
+}
+
+console.log(checkEvenOddFn(10));
---
+++ beginner-level-problems/main copy 8.js
@@ -0,0 +1,6 @@
+function countVowels(value){
+
+   return value.split("").length;
+}
+
+console.log(countVowels("132rc"));
