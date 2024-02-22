f68d3f82b706ba6b0b054a197f2de80236c4c96f
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-02-22 07:44:47 +0500

    learning

---
+++ beginner-level-problems/main copy 13.js
@@ -0,0 +1,11 @@
+let arr = [2, 5, 6, 7, 8, 11, 14, 15, 18, 21, 22, 26];
+
+
+function reverseArrFn(reverseArr) {
+
+   let reversedArr = reverseArr.reverse();
+
+   return reversedArr;
+}
+
+console.log(reverseArrFn(arr));
