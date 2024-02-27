00a1890b39d34c589f34b3e206b85594f2bd039d
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-02-27 14:48:03 +0500

    learning

--- beginner-level-problems-2 copy 2/main.js
+++ beginner-level-problems-2 copy 2/main.js
@@ -1,11 +1,49 @@
-function celcToFahr(n) {
+function calcGrade(n) {
 
-    return ((n * 9 / 5) + 32.0);
+    if (n >= 90 && n <= 100) {
 
+        return "A";
+    }
 
+    else if (n >= 80 && n < 90) {
+
+        return "B"
+    }
+
+    else if (n >= 70 && n < 80) {
+
+        return "C"
+    }
+
+    else if (n >= 60 && n < 70) {
+
+        return "D"
+    }
+
+    else if (n >= 50 && n < 60) {
+
+        return "E"
+    }
+
+    else if (n >= 0 && n < 50) {
+
+        return "F"
+    }
+
+    else {
+
+        return "Invalid Percentage";
+    }
 }
 
-let celsiusTemp = prompt("convert celsius to fahrenheit", 10);
+let yourPercentage = prompt("Enter your percentage:", 78);
 
+if (!isNaN(yourPercentage)) {
 
-alert(celcToFahr(celsiusTemp));
+    alert(calcGrade(yourPercentage));
+}
+
+else {
+
+    alert("Please enter valid Percentage");
+}
