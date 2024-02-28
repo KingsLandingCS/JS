1dc193e401d23ebed0a2091d532921bc50f36a7e
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-02-28 19:31:07 +0500

    learning

--- BMI-Calculator/main.js
+++ BMI-Calculator/main.js
@@ -1,15 +1,34 @@
-function calcBMIFn() {
+// let userWeight = prompt("Your weight");
 
-    let BMI = userWeight / Math.pow(userHeight, 2);
+// let userHeight = prompt("Your height");
 
-    alert(BMI);
+const calcBtn = document.querySelector(".calc-button");
+
+
+function calcBMIFn(weight, height) {
+
+    let BMI = weight / Math.pow(height, 2);
+
+    alert("Your BMI is: " + BMI);
 }
 
 
+calcBtn.addEventListener("click", function () {
 
-let userWeight = prompt("Your weight");
+    let weightInput = document.getElementById("weight").value;
+    let heightInput = document.getElementById("height").value;
 
-let userHeight = prompt("Your height");
+    if (isNaN(weightInput) || isNaN(heightInput) || weightInput <= 0 || heightInput <= 0) {
+
+        alert("Invalid value, please enter correct values.");
+        return;
+    }
+
+    calcBMIFn(weightInput, heightInput);
+
+});
 
 
-calcBMIFn();
+
+
+
