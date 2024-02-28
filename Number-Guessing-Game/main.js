0f0f6c223b4eb07a21f57dd45a26bfff74934d77
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-02-28 23:25:30 +0500

    learning

--- Number-Guessing-Game/main.js
+++ Number-Guessing-Game/main.js
@@ -1,13 +1,29 @@
 const randomNumber = Math.floor(Math.random() * 100) + 1;
+const guessBtn = document.querySelector(".guess-game");
 
 
-function guessNumber() {
+
+guessBtn.addEventListener("click", function () {
 
     let userInput;
 
     while (randomNumber !== userInput) {
 
         userInput = prompt("Your guess?");
+
+        if (userInput === null) {
+
+            alert("Game stopped");
+            return;
+        }
+
+        else if (isNaN(userInput)) {
+
+            alert("Please enter a valid number");
+            continue;
+        }
+
+        userInput = parseInt(userInput);
 
         if (userInput < randomNumber) {
 
@@ -21,6 +37,9 @@ function guessNumber() {
     }
 
     alert("Congrats! Your guess is correct!");
-}
 
-guessNumber();
+
+});
+
+
+
