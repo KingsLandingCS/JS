387ffa16997e5d18d4e3bf6dd6eaaa992d543343
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-03-01 16:14:49 +0500

    learning

--- Odd-or-Even/main.js
+++ Odd-or-Even/main.js
@@ -1,20 +1,44 @@
+const checkBtn = document.querySelector(".check");
+
 
 function myFunction(value) {
 
-    if (value % 2 === 0) {
 
-        return "even";
+    value = parseInt(value);
+
+    if (isNaN(value)) {
+
+        alert("UserInput is not a number");
+        return;
+    }
+
+    if (value <= 0) {
+        alert("UserInput is either zero or below zero");
+        return;
+    }
+
+    else if (value % 2 === 0) {
+
+        alert("even");
 
     } else {
 
-        return "odd";
+        alert("odd");
     }
 
 }
 
-let userInput = prompt("Please enter number:", 7);
+checkBtn.addEventListener("click", function () {
+
+    let userInput = prompt("Please enter number:");
+
+    alert(myFunction(userInput));
+
+});
 
 
-alert(myFunction(userInput));
+
+
+
 
 
