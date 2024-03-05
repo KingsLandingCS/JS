afe3ae3ecad175d0a95e6ad71aa25b0f84d28a19
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-03-05 21:18:48 +0500

    learning

---
+++ bubble-sort-algorithm/main.js
@@ -0,0 +1,25 @@
+function bubbleSort(arr) {
+
+    const n = arr.length;
+
+
+    for (let i = 0; i < n - 1; i++) {
+
+        for (let j = 0; j < n - 1 - 1; j++) {
+
+            if (arr[j] > arr[j + 1]) {
+
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+
+    }
+
+    return arr;
+
+}
+
+const numbers = [5, 3, 8, 1, 2];
+
+console.log("Original array: ", numbers);
+console.log("Sorted array:", bubbleSort(numbers.slice()));
