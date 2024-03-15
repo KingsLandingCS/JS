760653e7f907a5cd7bd19e8523efde669adbaf1c
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-03-15 20:09:02 +0500

    learning

---
+++ downward-left-aligned-triangle/main.js
@@ -0,0 +1,18 @@
+function drawTriangle(t){
+
+    for(let i = 1; i <= t; i++){
+
+        let eachLine = ""
+
+        for (let j = 1; j <= i; j++){
+
+            eachLine += j + " "
+        }
+
+        eachLine = eachLine.trim();
+        console.log(eachLine);
+    }
+}
+
+let t = 5;
+drawTriangle(t);
