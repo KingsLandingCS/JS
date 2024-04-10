488c81368f94b5a24c4086bd99593c246c9df706
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-04-10 22:30:18 +0500

    learning

---
+++ Clock/main.js
@@ -0,0 +1,14 @@
+let wakeUpTime = 7;
+let noon = 12;
+let lunchTime = 12;
+let napTime = lunchTime + 2;
+let partyTime;
+let evening = 18;
+
+//Getting it to show the current time on the page
+let showCurrentTime = function () {
+
+    //display the string on the webpage
+    let clock = document.getElementById("clock");
+
+}
